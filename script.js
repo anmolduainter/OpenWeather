e70381e4ad4ea49f8bc2fc9e9061b130751836c8
anmolduainter <anmolduainter@gmail.com>
2017-06-13 13:53:09 +0530

    .

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var APPID="53d7b6c5ae9a22f2a044a11865b41222";
 var temp;
 var icon;
+var main;
+var description;
 
 function updateByZip(zip){
 
@@ -18,6 +20,8 @@ function sendRequest(url){
               var weather={};
               weather.temp=data.main.temp;
               weather.icon=data.weather[0].icon;
+              weather.main=data.weather[0].main;
+              weather.desc=data.weather[0].description;
               console.log(weather.icon);
               update(weather);
 		}
@@ -31,6 +35,8 @@ function sendRequest(url){
 function update(weather){
 
    temp.innerHTML=weather.temp;
+   main.innerHTML=weather.main;
+   description.innerHTML=weather.desc;
    icon.src="http://openweathermap.org/img/w/"+weather.icon+".png";
    console.log(icon.src);
 
@@ -40,6 +46,8 @@ window.onload=function(){
 
     temp=document.getElementById('temp');
     icon=document.getElementById('icon');
-    updateByZip('Sydney');
+    main=document.getElementById('main');
+    description=document.getElementById('description');
+    updateByZip("london");
 
 }
